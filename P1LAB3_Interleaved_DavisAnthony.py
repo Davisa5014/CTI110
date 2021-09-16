cab56219b91f644d093802c1d1a90a21a3773529
Davisa5014 <90853008+Davisa5014@users.noreply.github.com>
2021-09-16 11:49:32 -0400

    First Submission for P1LAB3 Interleaved

---
+++ P1LAB3_Interleaved_DavisAnthony.py
@@ -0,0 +1,11 @@
+# CTI 110
+# P1LAB3
+# Anthony Davis
+# 16 Sep 2021
+
+# This is from Zybooks 7.13
+
+print('Enter x: ')
+x = int(input())
+# Removed print of output from user input
+print('x doubled is:', (2 * x))

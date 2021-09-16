2a31b0ffb010907112d3ec50d5c0f8e6ee1e1e2d
Davisa5014 <90853008+Davisa5014@users.noreply.github.com>
2021-09-16 11:00:41 -0400

    Add files via upload
    First Submission for P1LAB1

---
+++ P1LAB1_Davis.py
@@ -0,0 +1,11 @@
+# CTI 110
+# P1LAB1
+# Anthony Davis
+# 16 Sep 2021
+
+# This is 7.12 from Zybooks
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Math Bug Fixed
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed

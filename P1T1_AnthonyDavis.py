8fcb1b1ba0fe41278f9eab4ee7b63d8b28a7651b
Davisa5014 <90853008+Davisa5014@users.noreply.github.com>
2021-09-23 11:37:17 -0400

    P1T1_AnthonyDavis Replit Submission

---
+++ P1T1_AnthonyDavis.py
@@ -0,0 +1,11 @@
+# CTI 110
+# P1T1
+# Anthony Davis
+# 14 Sep 2021
+
+name = input("What is your name: ")
+print('Hi,', name)
+
+age =  int(input('How old are you? '))
+age = age + 1
+print ('Next year you will be', age)

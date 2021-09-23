393ba3f73f132202080ddcfb767e55f736f70ea5
Davisa5014 <90853008+Davisa5014@users.noreply.github.com>
2021-09-23 11:30:00 -0400

    P1HW1 Submission Variables

---
+++ P1HW1_DavisAnthony.py
@@ -0,0 +1,14 @@
+# CTI - 110
+# P1HW1
+# ANTHONY DAVIS
+# 21 SEP 2021
+
+# Outputting with different variables
+
+userNum = int(input('Enter integer:\n'))
+print ('You entered:', userNum)
+print (userNum, 'squared is', userNum * userNum)
+print ('And', userNum, 'cubed is', userNum * userNum * userNum, '!!')
+userNum2 = int(input('Enter another integer:\n'))
+print (userNum, '+', userNum2, 'is', userNum + userNum2)
+print (userNum, '*', userNum2, 'is', userNum * userNum2)

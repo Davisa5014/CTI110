c34d9e3794563f29694e030a69415a029bfc7e41
Davisa5014 <90853008+Davisa5014@users.noreply.github.com>
2021-09-23 11:31:46 -0400

    P1T2 Movie Blurb

---
+++ P1T2_AnthonyDavis.py
@@ -0,0 +1,19 @@
+# CTI - 110
+# P1T2
+# ANTHONY DAVIS
+# 21 SEP 2021
+
+movieName = ('"The Usual Suspects"')
+year = int(1995)
+boxOffice = float(34.4)
+quote = """
+Verbal:
+The greatest trick the devil
+ever pulled was convincing
+the world he did not exist.
+And like that... he is gone.
+"""
+print ("The name of the movie is", movieName)
+print ("The movie came out in", year)
+print ("It made $"+str (boxOffice), "million")
+print ("A famous quote from the movie is", quote)

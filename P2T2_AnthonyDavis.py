0998db06dd8df69664002595390fe8f8fd9b72ea
Davisa5014 <90853008+Davisa5014@users.noreply.github.com>
2021-10-05 11:07:16 -0400

    ELIF statement code

---
+++ P2T2_AnthonyDavis.py
@@ -0,0 +1,14 @@
+# P2T2 - The White Claw Problem
+# CTI - 110
+# Anthony Davis
+# 5 Oct 2021
+
+# Find out if someone can buy some White Claw
+
+age = (input ('What year were you born? '))
+if age < '2000':
+    print ('You can buy some White Claw!')
+elif age == '2000':
+    print ('I need to check your ID.')
+else:
+    print ('You are too young!')

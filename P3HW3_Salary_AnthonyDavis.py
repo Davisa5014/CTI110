480990014d07a434fbd965fc51752ecabc3a385e
Davisa5014 <90853008+Davisa5014@users.noreply.github.com>
2021-10-17 11:27:24 -0400

    Salary Calculator

---
+++ P3HW3_Salary_AnthonyDavis.py
@@ -0,0 +1,47 @@
+# CTI - 110
+# P3HW2 - Salary
+# Anthony Davis
+# 14 Oct 2021
+
+# Asks the user to name of employee
+# Ask user to enter number of hours the employee worked this month
+# Ask user to enter employee's pay rate
+# Evaluate if employee has worked overtime. If yes calcuate amount owed to employee for overtime hours
+# Calculate amount employee shoud be paid for regular hours worked.
+# Display gross pay (total amount that should be paid to employee)
+# The program is to display the following ( Employee name, pay rate,
+# number of hours worked, overtime hours, overtime pay , pay for regular hours and gross pay).
+
+name = input("Please enter employee's name: ")
+hours_worked = float(input('Please enter hours worked this MONTH: '))
+pay_rate = float(input("Please enter employee's pay rate: "))
+print ('------------------------------------------')
+
+reg_pay = hours_worked * pay_rate # Regular pay
+over_time_rate = pay_rate * 1.5 # Time and a half pay
+
+if hours_worked <= 160: # Formula for over time hours.
+    over_time = 0
+elif (hours_worked > 160) and (hours_worked < 320):
+    over_time = hours_worked % 160
+else:
+    over_time = hours_worked - 160
+
+over_time_pay = over_time * over_time_rate # Over time pay
+gross_pay = reg_pay + over_time_pay # Total pay
+
+
+print ('Employee Name: ', name)
+print ('------------------------------------------')
+print ('Hours Worked: '+str(format (hours_worked, '.2f')))
+print ('------------------------------------------')
+print ('Pay Rate: $'+str(format (pay_rate, '.2f')))
+print ('------------------------------------------')
+print ('Over Time: '+str(format (over_time, '.2f')))
+print ('------------------------------------------')
+print ('Over Time Pay: $'+str(format (over_time_pay, '.2f')))
+print ('------------------------------------------')
+print ('Regular Hour Pay: $'+str(format (reg_pay, '.2f')))
+print ('------------------------------------------')
+print ('Gross Pay: $'+str(format (gross_pay, '.2f')))
+print ('------------------------------------------')
